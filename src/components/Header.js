5df4c1cf5bd1ef4c698e0d195cbbafa449595b85
Risky Satria <riskysatria666@gmail.com>
2021-03-31 12:22:41 +0700

    Adding logo and some adjustment styling in header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,13 @@
 import PropTypes from 'prop-types'
 import React from 'react'
+import Logo from '../images/logo.png'
 
 const Header = props => (
   <header id="header" style={props.timeout ? { display: 'none' } : {}}>
     <div className="logo">
-      <span className="icon fa-diamond"></span>
+      <span className="icon">
+        <img src={Logo} alt="logo" />
+      </span>
     </div>
     <div className="content">
       <div className="inner">
